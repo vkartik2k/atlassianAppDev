8f80c896a98802cf69663b634b161380f8e0d37f
Mandeep Singh <mandeepsrr@gmail.com>
2021-02-23 19:48:07 +0530

    added logo

--- public/index.js
+++ public/index.js
@@ -24,20 +24,73 @@ let variables = {
 
 let templates = {
 }
+var myGamePiece;
+
+
+
+
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    if (type == "image") {
+        this.image = new Image();
+        this.image.src = color;
+    }
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        ctx = myGameArea.context;
+        if (type == "image") {
+            ctx.drawImage(this.image, 
+                this.x, 
+                this.y,
+                this.width, this.height);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }    
+}
+
+
+function move(dir) {
+    myGamePiece.image.src = "angry.gif";
+    if (dir == "up") {myGamePiece.Yspeed = -1; }
+    if (dir == "down") {myGamePiece.speedY = 1; }
+    if (dir == "left") {myGamePiece.speedX = -1; }
+    if (dir == "right") {myGamePiece.speedX = 1; }
+}
+
+function clearmove() {
+    myGamePiece.image.src = "smiley.gif";
+    myGamePiece.speedX = 0; 
+    myGamePiece.speedY = 0; 
+}
 
 function loadCanvas() {
-    let canvas = document.getElementById("main");
+    let canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
 
-    canvas.width = document.getElementById("main").width;
-    canvas.height = document.getElementById("main").height;
-    var background = new Image();
-    background.src = "http://www.samskirrow.com/background.png";
-
+    canvas.width = document.getElementById("canvas").width;
+    canvas.height = document.getElementById("canvas").height;
+    
+    var atlassianCard = new Image();
+    atlassianCard.src = "assets/atlassian.png";
+    atlassianCard.width=canvas.width;
+    atlassianCard.height=canvas.height;
     // Make sure the image is loaded first otherwise nothing will draw.
-    background.onload = function () {
-        ctx.drawImage(background, 0, 0);
+    atlassianCard.onload = function () {
+        ctx.drawImage(atlassianCard, 540, 340);
     }
+    
+
 }
 
 $(document).ready(function () {
@@ -52,7 +105,7 @@ $(document).ready(function () {
                     localStorage.setItem("user", JSON.stringify(data.user));
                     console.log(data.user)
                     $('.overlayLogin').hide()
-                    loadCanvas()
+                    
                 }
                 else {
                     console.log("Error")
@@ -60,5 +113,5 @@ $(document).ready(function () {
                 }
             });
     })
-
+    loadCanvas();
 })
