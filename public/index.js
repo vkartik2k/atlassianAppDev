3ab91dc8dd4589c750b41e0084e4316c67926cfa
Mandeep Singh <mandeepsrr@gmail.com>
2021-02-24 00:18:22 +0530

    movement of man

--- public/index.js
+++ public/index.js
@@ -18,85 +18,132 @@ async function postData(url = '', data = {}) {
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
 let variables = {
-    person : {
-        x : 10,
-        y : 20,
-        orientation : 0 
+    person: {
+        x: 10,
+        y: 20,
+        orientation: 0
     }
 }
-
 let templates = {
 }
-
-
-
+var coffeeshop, atlassianCard, person1, gym, stadium, office;
 function loadCanvas() {
     let canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
 
-    canvas.width = document.getElementById("canvas").width;
-    canvas.height = document.getElementById("canvas").height;
-    var counter = 3;
+    var camerax = 580, cameray = 390;
+    var fps = 60;
+    var img;
+    var loop;
 
-    // canvas.globalCompositeOperation = "destination-over";
-    var coffeeshop = new Image();
-    coffeeshop.src = "assets/coffeeshop.png";
-    coffeeshop.onload = function () {
+    function update() {
+    }
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawGround();
+        // cameray-=1;
+        // camerax+=1;
+
+
+    }
+
+    function drawGround() {
         ctx.drawImage(coffeeshop, 830, 160, 226.87, 165);
+        ctx.drawImage(gym, 330, 8, 250, 180);
+        ctx.drawImage(stadium, 440, 140, 354.6, 275);
+        ctx.drawImage(office, 180, 10, 224, 280);
+        ctx.drawImage(atlassianCard, 520, 380, 90, 90);
+        ctx.drawImage(person1, camerax, cameray, 35, 70);
     }
 
-    var gym = new Image();
-    gym.src = "assets/gym.png";
-    gym.onload = function () {
-        ctx.drawImage(gym, 330, 8, 250, 180);
+    function init() {
+      
+        canvas.width = document.getElementById("canvas").width;
+        canvas.height = document.getElementById("canvas").height;
+        var counter = 3;
+
+        // canvas.globalCompositeOperation = "destination-over";
+        coffeeshop = new Image();
+        coffeeshop.src = "assets/coffeeshop.png";
+        coffeeshop.onload = function () {
+            ctx.drawImage(coffeeshop, 830, 160, 226.87, 165);
+        }
+
+        gym = new Image();
+        gym.src = "assets/gym.png";
+        gym.onload = function () {
+            ctx.drawImage(gym, 330, 8, 250, 180);
+
+        }
+        stadium = new Image();
+        stadium.src = "assets/stadium.png";
+        stadium.onload = function () {
+            ctx.drawImage(stadium, 440, 140, 354.6, 275);
+
+        }
+        office = new Image();
+        office.src = "assets/office.png";
+        office.onload = function () {
+            ctx.drawImage(office, 180, 10, 224, 280);
+
+        }
+        person1 = new Image();
+        person1.src = "assets/person1.png";
+        person1.onload = function () {
+            counter--;
+            if (counter <= 0) {
+                ctx.drawImage(atlassianCard, 520, 380, 90, 90);
+                ctx.drawImage(person1, 580, 390, 35, 70);
+                
+            }
+        }
+
+        atlassianCard = new Image();
+        atlassianCard.src = "assets/atlassian.png";
+        atlassianCard.onload = function () {
+            counter--;
+            if (counter <= 0) {
+                ctx.drawImage(atlassianCard, 520, 380, 90, 90);
+                ctx.drawImage(person1, 580, 390, 35, 70);
         
-    }
-    var stadium = new Image();
-    stadium.src = "assets/stadium.png";
-    stadium.onload = function () {
-        ctx.drawImage(stadium, 440, 140, 354.6, 275);
-        
-    }
-    var office = new Image();
-    office.src = "assets/office.png";
-    office.onload = function () {
-        ctx.drawImage(office, 180, 10, 224, 280);
-        
-    }
-    var person1 = new Image();
-    person1.src = "assets/person1.png";
-    person1.onload = function () {
-        counter--;
-        if (counter<=0) {
-            ctx.drawImage(atlassianCard, 520, 380, 90, 90);
-            ctx.drawImage(person1, 580, 390, 35, 70);
-            ctx.drawImage(person2, 450, 340, 35, 70);
+            }
         }
     }
+    function keyListener(e){
+        e = e || window.event
+       
+        if(e.keyCode==37){
+            if (camerax >10) {
+                camerax -= 5;
+            }
+        }
+        else if(e.keyCode==39){
+            if (camerax < 700) {
+                camerax += 5;
+            }
+        }
+        else if(e.keyCode==38){
+            if (cameray > 10) {
+                cameray -= 5;
+            }
+        }
+        else if(e.keyCode==40){
+            if (cameray < 500) {
+                cameray += 5;
+            }
+        }                           
+     }
+
+     window.onkeydown = function(e) {
+         keyListener(e);
+     }
     
-    var person2 = new Image();
-    person2.src = "assets/person2.png";
-    person2.onload = function () {
-        counter--;
-        if (counter<=0) {
-            ctx.drawImage(atlassianCard, 520, 380, 90, 90);
-            ctx.drawImage(person1, 580, 390, 35, 70);
-            ctx.drawImage(person2, 450, 340, 35, 70);
-        }
-    }
-    var atlassianCard = new Image();
-    atlassianCard.src = "assets/atlassian.png";
-    atlassianCard.onload = function () {
-        counter--;
-        if (counter<=0) {
-            ctx.drawImage(atlassianCard, 520, 380, 90, 90);
-            ctx.drawImage(person1, 580, 390, 35, 70);
-            ctx.drawImage(person2, 450, 340, 35, 70);
-        }
-    }
-    
+    init();
+    var loop = setInterval(function () {
+        update();
+        draw();
+    }, 1000 / fps);
 
 }
 
@@ -104,7 +151,7 @@ $(document).ready(function () {
     // if (!localStorage.user && (typeof localStorage.user === 'undefined'))
     //  $('#overlayLogin').show()
     //  $('#overlayCollab').show()
-     $('#overlayAboutUs').show()
+    //  $('#overlayAboutUs').show()
 
     $('#loginBtn').click(() => {
         const email = $("#loginEmail").val()
